68657db8b552bc0b133a47a4b648a3f3a0640a72
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2021-10-25 10:53:18 +0900

    初期データの作成

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Event.create!(name: '予定1', start: '2021-07-14 10:00:00', end: '2021-07-14 11:00:00')
+Event.create!(name: '予定2', start: '2021-07-14 10:00:00', end: '2021-07-14 11:00:00')
+Event.create!(name: '予定3', start: '2021-07-14 10:00:00', end: '2021-07-14 11:00:00')
+Event.create!(name: '予定4', start: '2021-07-14 10:00:00', end: '2021-07-14 11:00:00')
